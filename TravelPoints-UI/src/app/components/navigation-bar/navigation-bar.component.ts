5b46a89a651d187519ef98c100b769a3cb24ea6f
Anca Chetan <anca.chetan1706@yahoo.com>
2024-04-11 09:15:57 +0300

    VDPN-21 VDPN-45 UI-create navigation bar

--- TravelPoints-UI/src/app/components/navigation-bar/navigation-bar.component.ts
+++ TravelPoints-UI/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {LoginDialogComponent} from "../login-dialog/login-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
+  token: any
+
   constructor(private dialog: MatDialog) {
+    this.token = localStorage.getItem('token')
   }
 
   openLoginDialog() {
