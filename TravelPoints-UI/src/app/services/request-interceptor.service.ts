de2b180cbeee9c0d80c5c63ee5afaf170c72effe
Andreea Dragus <dragusandreea33@yahoo.com>
2024-05-07 14:01:25 +0300

    VDPN-67 UI: create interceptor for http requests

---
+++ TravelPoints-UI/src/app/services/request-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RequestInterceptorService implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let headers = req.headers;
+    if (req.url.includes('/auth/register') || req.url.includes('/auth/login') || req.url.includes('/attractions/getAll')) {
+      headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+    } else {
+      let token = localStorage.getItem("token")
+      headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+    }
+    const authReq = req.clone({ headers });
+    return next.handle(authReq);
+  }
+}
