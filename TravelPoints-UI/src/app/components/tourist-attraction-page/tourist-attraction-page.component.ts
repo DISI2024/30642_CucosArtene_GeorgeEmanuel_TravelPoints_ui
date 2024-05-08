cb87519edf74c5e9e5ab0517f1ad51fa285bf508
Anca Chetan <anca.chetan1706@yahoo.com>
2024-05-08 13:41:41 +0300

    VDPN-16 VDPN-103 add wishlist button with logic for tourist attraction cards

--- TravelPoints-UI/src/app/components/tourist-attraction-page/tourist-attraction-page.component.ts
+++ TravelPoints-UI/src/app/components/tourist-attraction-page/tourist-attraction-page.component.ts
@@ -10,84 +10,112 @@ import {MatInput} from "@angular/material/input";
 import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
 import {TouristAttraction} from "../../models/TouristAttraction";
 import {
-  TouristAttractionDetailsDialogComponent
+    TouristAttractionDetailsDialogComponent
 } from "../tourist-attraction-details-dialog/tourist-attraction-details-dialog.component";
 import {HttpClientModule} from "@angular/common/http";
 import {TouristAttractionService} from "../../services/tourist-attraction.service";
+import {Wishlist} from "../../models/Wishlist";
+import {jwtDecode} from "jwt-decode";
 
 @Component({
-  selector: 'app-objectives-page',
-  standalone: true,
-  imports: [
-    NavigationBarComponent,
-    NgForOf,
-    MatFormField,
-    MatLabel,
-    FormsModule,
-    MatIconButton,
-    MatIcon,
-    MatInput,
-    MatRadioButton,
-    NgIf,
-    MatRadioGroup,
-    HttpClientModule
-  ],
-  providers: [TouristAttractionService],
-  templateUrl: './tourist-attraction-page.component.html',
-  styleUrl: './tourist-attraction-page.component.css'
+    selector: 'app-objectives-page',
+    standalone: true,
+    imports: [
+        NavigationBarComponent,
+        NgForOf,
+        MatFormField,
+        MatLabel,
+        FormsModule,
+        MatIconButton,
+        MatIcon,
+        MatInput,
+        MatRadioButton,
+        NgIf,
+        MatRadioGroup,
+        HttpClientModule
+    ],
+    providers: [TouristAttractionService],
+    templateUrl: './tourist-attraction-page.component.html',
+    styleUrl: './tourist-attraction-page.component.css'
 })
 export class TouristAttractionPageComponent implements OnInit {
-  touristAttractions: TouristAttraction[] = []
-  touristAttractionsResult: TouristAttraction[] = []
-  selectedOption: string | undefined
-  searchInput: string | undefined
+    touristAttractions: TouristAttraction[] = []
+    touristAttractionsResult: TouristAttraction[] = []
+    selectedOption: string | undefined
+    searchInput: string | undefined
+    token: any
+    loggedUserId: number | undefined
+    loggedUserType: string | undefined
+    wishlist: Wishlist =
+        {
+            wishlistId: 1,
+            userId: 2,
+            attractionIds: [1, 2],
+            addedAt: '2024-08-25'
+        }
 
-  constructor(private dialog: MatDialog,
-              private touristAttractionService: TouristAttractionService) {
-  }
-
-  ngOnInit() {
-    this.selectedOption = 'category'
-    this.touristAttractionService.getAllTouristAttractions().subscribe({
-      next: data => {
-        this.touristAttractions = data
-        this.touristAttractionsResult = this.touristAttractions
-      }
-    })
-  }
-
-  openDetailsDialog(details: string) {
-    this.dialog.open(TouristAttractionDetailsDialogComponent, {
-      width: '100vh',
-      data: details,
-    });
-  }
-
-  onChoice1Change(event: any) {
-    this.selectedOption = (event.target as HTMLInputElement).value;
-  }
-
-  onChoice2Change(event: any) {
-    this.selectedOption = (event.target as HTMLInputElement).value;
-  }
-
-  filterByCategory(category: string) {
-    this.touristAttractionsResult = this.touristAttractions.filter(touristAttraction => touristAttraction.category!.valueOf().toLowerCase().includes(category.toLowerCase()))
-  }
-
-  filterByLocation(location: string) {
-    this.touristAttractionsResult = this.touristAttractions.filter((touristAttraction) => touristAttraction.location?.toLowerCase().includes(location.toLowerCase()))
-  }
-
-  onInputChange() {
-    if (this.searchInput === undefined || this.searchInput === '') {
-      this.touristAttractionsResult = this.touristAttractions
-    } else {
-      if (this.selectedOption === "category") {
-        this.filterByCategory(this.searchInput!)
-      } else {
-        this.filterByLocation(this.searchInput!)
-      }
+    constructor(private dialog: MatDialog,
+                private touristAttractionService: TouristAttractionService) {
     }
-  }
+
+    ngOnInit() {
+        this.token = localStorage.getItem('token')
+        let tokenPayload: any;
+        if (this.token) {
+            tokenPayload = jwtDecode(this.token);
+            this.loggedUserId = tokenPayload.id;
+            this.loggedUserType = tokenPayload.userType;
+        }
+        this.selectedOption = 'category'
+        this.touristAttractionService.getAllTouristAttractions().subscribe({
+            next: data => {
+                this.touristAttractions = data
+                this.touristAttractionsResult = this.touristAttractions
+            }
+        })
+    }
+
+    openDetailsDialog(details: string) {
+        this.dialog.open(TouristAttractionDetailsDialogComponent, {
+            width: '100vh',
+            data: details,
+        });
+    }
+
+    onChoice1Change(event: any) {
+        this.selectedOption = (event.target as HTMLInputElement).value;
+    }
+
+    onChoice2Change(event: any) {
+        this.selectedOption = (event.target as HTMLInputElement).value;
+    }
+
+    filterByCategory(category: string) {
+        this.touristAttractionsResult = this.touristAttractions.filter(touristAttraction => touristAttraction.category!.valueOf().toLowerCase().includes(category.toLowerCase()))
+    }
+
+    filterByLocation(location: string) {
+        this.touristAttractionsResult = this.touristAttractions.filter((touristAttraction) => touristAttraction.location?.toLowerCase().includes(location.toLowerCase()))
+    }
+
+    onInputChange() {
+        if (this.searchInput === undefined || this.searchInput === '') {
+            this.touristAttractionsResult = this.touristAttractions
+        } else {
+            if (this.selectedOption === "category") {
+                this.filterByCategory(this.searchInput!)
+            } else {
+                this.filterByLocation(this.searchInput!)
+            }
+        }
+    }
+
+    checkInWishlist(attractionId: number): boolean {
+        const foundItem = this.wishlist.attractionIds.find(id => id === attractionId);
+        return !!foundItem;
+    }
+
+    addToWishlist(attractionId: number) {
+        this.wishlist.attractionIds.push(attractionId)
+    }
 }
---
+++ TravelPoints-UI/src/app/models/Wishlist.ts
@@ -0,0 +1,6 @@
+export class Wishlist {
+    wishlistId: number | undefined;
+    userId: number | undefined;
+    attractionIds: number[] = [];
+    addedAt: string | undefined;
+}
